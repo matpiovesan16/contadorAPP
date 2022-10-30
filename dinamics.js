af8ec113b325493eb8ebe4ab50bdcf6a22cc76e3
Matias Piovesan <piovesanmatias@gmail.com>
2022-10-30 16:06:25 -0300

    Contador con JS

---
+++ dinamics.js
@@ -0,0 +1,58 @@
+(function () {
+    var contador = 0;
+
+    var sumar = function () {
+        contador++;
+
+        if (contador < 10 && contador > 0) {
+            cuenta.innerHTML = "0" + contador;
+
+        } else {
+            cuenta.innerHTML = contador;
+
+        }
+
+    }
+
+
+    var restar = function () {
+        contador--;
+
+        if (contador < 10 && contador > 0) {
+            cuenta.innerHTML = "0" + contador;
+
+        }
+
+        if (contador==0){
+            cuenta.innerHTML='0'+contador;
+        }
+
+        if(contador<0){
+            cuenta.innerHTML=contador;
+        }
+
+    }
+
+
+    var reset = function () {
+        contador = 0;
+        cuenta.innerHTML = '0'+contador;
+    }
+
+    var suma = document.getElementById('sumar');
+    suma.addEventListener("click", sumar);
+    var cuenta = document.getElementById("cuenta");
+
+    var resta = document.getElementById("resta");
+    resta.addEventListener("click", restar);
+
+    var restard = document.getElementById("reset");
+    restard.addEventListener("click", reset);
+
+}())
+
+
+
+
+
+
